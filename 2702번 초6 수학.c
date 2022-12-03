9ec508ab416c890d135318a39a7bc808157a95f4
ChaGyoungtae <113877917+ChaGyoungtae@users.noreply.github.com>
2022-12-03 21:28:27 +0900

    [C]2702번 초6 수학
    최대공약수 최소공배수

---
+++ 2702번 초6 수학.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	int t;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &a, &b);
+		if (a > b) c = b;
+		else c = a;
+		int n = 1, m = 1;
+
+		int i = 2;
+		while (1) {
+			if (a % i == 0 && b % i == 0) {
+				a /= i;
+				b /= i;
+				n *= i;
+			}
+			else {
+				i++;
+				if (i > a || i > b) break;
+			}
+		}
+		printf("%d %d", n*a*b, n);
+	}
+
+
+	
+
+}
